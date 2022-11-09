13de450e34e8ff2ee102ed8e1f3312c8deb88096
Tara Tohidi <tara.tohidi@gmail.com>
2022-11-09 12:54:16 -0800

    Create partial template and generate links

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -9,19 +9,39 @@ use Symfony\Component\Routing\Annotation\Route;
 //  extends AbstractController make twig file of controller
 class HelloController extends AbstractController
 {
-    private array $messages = ['Hi', 'Hello', 'Good Morning', "bye", 'hola'];
+    private array $messages = [
+        ['message' => 'Hello', 'created' => '2022/09/12'],
+        ['message' => 'Hi', 'created' => '2022/08/12'],
+        ['message' => 'Bye!', 'created' => '2021/05/12']
+      ];
 
 // option you can specify how many items to be return:({limit<\d+>?3}) 
 
-    #[Route('/{limit<\d+>?4}', name: 'app_hello')]
+    // #[Route('/{limit<\d+>?4}', name: 'app_hello')]
+    // public function index(int $limit): Response
+    // {
+    //     // return new Response(implode(',', array_slice($this-> messages,0, $limit)));
+    //     return $this -> render(
+    //         'hello/index.html.twig',
+    //         ['messages'=>  array_slice($this-> messages,0, $limit)]
+    //     );
+    // }
+
+    #[Route('/{limit<\d+>?3}', name: 'app_hello')]
     public function index(int $limit): Response
     {
         // return new Response(implode(',', array_slice($this-> messages,0, $limit)));
         return $this -> render(
             'hello/index.html.twig',
-            ['messages'=>  array_slice($this-> messages,0, $limit)]
+            [
+                'messages'=>  $this-> messages,
+                'limit' => $limit
+            ]
         );
     }
+
+
+
 
     // /hello/{id\d+>} it means it must be integer if not return 404 error
 
